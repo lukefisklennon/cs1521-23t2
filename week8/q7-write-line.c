d99600aab3fc9bb01cd78d736c58960c182e0995
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2023-07-17 19:57:29 +1000

    Week 8

---
+++ week8/q7-write-line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return EXIT_FAILURE; // 1
+	}
+
+	FILE *file = fopen(argv[1], "w");
+	if (file == NULL) {
+		perror(argv[1]);
+		return EXIT_FAILURE; // 1
+	}
+
+	int c;
+	while ((c = fgetc(stdin)) != EOF) {
+		fputc(c, file);
+
+		if (c == '\n') {
+			break;
+		}
+	}
+
+	fclose(file);
+}
