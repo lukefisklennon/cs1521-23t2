1d832d4e521aa2415e2afb886bf6f99dd4a7a10e
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2023-07-24 20:17:28 +1000

    Week 9 comments

--- week9/q1-print-diary.c
+++ week9/q1-print-diary.c
@@ -5,28 +5,36 @@
 #define DIARY_FILE ".diary"
 
 int main(int argc, char *argv[]) {
+	// Get the value of the $HOME envrionment variable, which is equivalent to "~" in the shell
 	char *home_path = getenv("HOME");
+	// Use ".", the current directory, as a fallback
 	if (home_path == NULL) {
 		home_path = ".";
 	}
 
+	// Calculate the amount of memory needed (size of $HOME + size of "/" + size of "./diary" + null terminator)
 	int path_length = strlen(home_path) + strlen(DIARY_FILE) + 2;
+	// Allocate the memory for the final pathname
 	char *diary_path = malloc(path_length);
-
+	// Put the paths together and output it to that new memory
 	snprintf(diary_path, path_length, "%s/%s", home_path, DIARY_FILE);
 
+	// Open the file and print an error message if needed
 	FILE *diary_file = fopen(diary_path, "r");
 	if (diary_file == NULL) {
 		perror(diary_path);
 		return EXIT_FAILURE;
 	}
 
+	// Loop through the file and print out each character
 	int c;
 	while ((c = fgetc(diary_file)) != EOF) {
 		fputc(c, stdout);
 	}
 
+	// Close the file once done
 	fclose(diary_file);
+	// Free our memory once done
 	free(diary_path);
 
 	return EXIT_SUCCESS;
--- week9/q4-chmod_if_public_write.c
+++ week9/q4-chmod_if_public_write.c
@@ -8,9 +8,11 @@ void chmod_if_public_write(char *path);
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
+		// Print an error to standard error if needed
 		fprintf(stderr, "Please provide 1 or more arguments");
 	}
 
+	// Loop through the arguments and process each one
 	int i = 0;
 	while (i < argc) {
 		chmod_if_public_write(argv[i]);
@@ -21,15 +23,20 @@ int main(int argc, char *argv[]) {
 }
 
 void chmod_if_public_write(char *path) {
+	// Get the file metadata and put it into our struct
 	struct stat stats;
 	int result = stat(path, &stats);
+	// Check if the function worked and print an error if needed
 	if (result != 0) {
 		perror(path);
 		return;
 	}
 
+	// Check if writeable ("W") by others ("OTH") using the provided bit mask
 	if (stats.st_mode & S_IWOTH) {
+		// Flip that bit to a 0 if needed (negate the mask so that bit becomes 0 and everything else becomes 1)
 		mode_t new_mode = stats.st_mode & ~S_IWOTH;
+		// Change ("ch") the mode ("mod"), and print an error if needed
 		int result = chmod(path, new_mode);
 		if (result != 0) {
 			perror(path);
